639c637ad951676b3ce92d98e71f9f834029380c
yatharth <yatharthrohida522@gmail.com>
2026-05-31 01:53:38 +0530

    v6

---
+++ basics/conversionoperation.js
@@ -0,0 +1,29 @@
+let score="33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 ,false=>0
+
+let isloggedin="yatharth"
+let booleanisloggedin=Boolean(isloggedin)
+
+console.log(booleanisloggedin);
+
+//1=>true;0=>false
+//""=>false
+//"yatharth"=>true
+
+let somenumber = 33
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
