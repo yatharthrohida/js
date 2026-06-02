5aaceaac698e3ee6a6670be3e29d457ace015310
yatharth <yatharthrohida522@gmail.com>
2026-06-03 00:27:22 +0530

    v12,v13

---
+++ basics/06nums_and_maths.js
@@ -0,0 +1,28 @@
+const score=700
+const balance=new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length)
+//console.log(balance.toFixed(1))
+
+const othernumber=123.987338
+//console.log(othernumber.toPrecision(3))
+
+const hundred=1000000
+//console.log(hundred.toLocaleString('en-IN'))
+
+//+++++++++++++++maths++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.1));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(9,7,4,2));
+
+//math.random() picks value between 0 and 1
+console.log(Math.floor(Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+const min=1
+const max=6
+console.log(Math.floor(Math.random() * (max-min+1))+min)
